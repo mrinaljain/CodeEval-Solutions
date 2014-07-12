5161e7e413d047bc954215356813b4b0d64859df
Bert Mouler <bmouler@gmail.com>
2014-07-12 11:02:42 -0700

    Added corrections.

--- CodeEval - Happy Numbers/HappyNumbers.py
+++ CodeEval - Happy Numbers/HappyNumbers.py
@@ -8,13 +8,20 @@ import sys
 test_cases = open(sys.argv[1], 'r')
 
 for test in test_cases:
-    if int(test) == 1: print 1
-    else:
-        l = list(map(int, test.strip()))
-        print l
-        s = 0
-        for num in l: s += num*num
-        l = s
-            while s != 1:
-                continue
+    num = map(int, list(test.strip()))
+#     print num
+    temp = []
+    count = 0
+    while len(num) != 1 and num[0] != 1 and count < 1000000000000000:
+        t = 0
+        for n in num:
+            t += n*n
+#             print t,
+        num = map(int, list(str(t)))
+        count += 1
+#         print int(''.join(str(e) for e in num))
+        
+    if int(''.join(str(e) for e in num))==1:
+        print 1
+    else: print 0
 test_cases.close()

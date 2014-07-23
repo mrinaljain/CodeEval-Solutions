f0b066cbb35bbadb295b91510fd19ac633a6d148
Bert Mouler <bmouler@gmail.com>
2014-07-23 09:08:54 -0700

    Added working solution.

--- CodeEval - Happy Numbers/HappyNumbers.py
+++ CodeEval - Happy Numbers/HappyNumbers.py
@@ -8,20 +8,16 @@ import sys
 test_cases = open(sys.argv[1], 'r')
 
 for test in test_cases:
-    num = map(int, list(test.strip()))
-#     print num
-    temp = []
-    count = 0
-    while len(num) != 1 and num[0] != 1 and count < 1000000000000000:
-        t = 0
-        for n in num:
-            t += n*n
-#             print t,
-        num = map(int, list(str(t)))
-        count += 1
-#         print int(''.join(str(e) for e in num))
-        
-    if int(''.join(str(e) for e in num))==1:
-        print 1
-    else: print 0
+    num = int(test.strip())
+    visited = set()
+    while 1:
+        if num == 1:
+            print 1
+            break
+        num = sum(int(n) ** 2 for n in str(num))
+        if num in visited:
+            print 0
+            break
+        visited.add(num)
+
 test_cases.close()
